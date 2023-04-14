88fac22c011367a4b3cdc9690eebeb225804f8c4
Vedant2s <98942578+Vedant2s@users.noreply.github.com>
2023-04-14 10:20:29 +0530

    Added Solution - GfG to GitHub

---
+++ School/Second Largest/second-largest.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	// Function returns the second
+	// largest elements
+	int print2largest(int arr[], int n) {
+	    // code here
+	    set<int> s;
+	    sort(arr,arr+n);
+	    for(int i=0;i<n;i++){
+	        s.insert(arr[i]);
+	    }
+	    
+	    if(s.size()>=2)
+	    {auto it = next(s.begin(), s.size()-2);
+	    return *it;}
+     
+    // Printing the element at index n
+    //cout<<s.size();
+    //if(s.size()<=2)
+    return -1;
+    
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.print2largest(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
